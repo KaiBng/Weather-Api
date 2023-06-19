1c5cb7f207ccf99efc207c81126c500b5bc19abe
KaiBng <136330864+KaiBng@users.noreply.github.com>
2023-06-19 15:55:06 +0200

    Update script.js
    Sunrise and Sunset set to Ghost because of current Problems

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let weather = {
-    apiKey: "YOUR_API_KEY",
+    apiKey: "644db26edfd1b5964dfd3815bbfc27ff",
     fetchWeather: function (city) {
       fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -15,20 +15,30 @@ let weather = {
           return response.json();
         })
         .then((data) => this.displayWeather(data));
+        
     },
+
+    /* data request sending and fetching variable datas */
     displayWeather: function (data) {
       const { name } = data;
       const { icon, description } = data.weather[0];
       const { temp, humidity } = data.main;
       const { speed } = data.wind;
+    /*  const { sunrise } = data.sys;
+      const { sunset } = data.sys; */
+      
+/* showing (echoing) the fetched data */
       document.querySelector(".city").innerText = "" + name;
       document.querySelector(".icon").src =
-        "https://openweathermap.org/img/wn/" + icon + ".png";
+        "https://openweathermap.org/img/wn/" + icon + ".png"; 
+        document.querySelector(".description").innerText = "" + description;
       document.querySelector(".temp").innerText = temp + "°C";
       document.querySelector(".humidity").innerText =
         "Feuchtigkeit: " + humidity + "%";
       document.querySelector(".wind").innerText =
-        "Wind Geschwindigkeit: " + speed + " km/h";
+        "Windgeschwindigkeit: " + speed + " km/h";
+     /* document.querySelector(".sunrise").innerText = "Sonnenaufgang: " + sunrise;
+      document.querySelector(".sunset").innerText = "Sonnenuntergang: " + sunset; */
       document.querySelector(".weather").classList.remove("loading");
       document.body.style.backgroundImage =
         "url('https://source.unsplash.com/1600x900/?" + name + "')";
@@ -41,7 +51,7 @@ let weather = {
   document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
   });
-  
+  /* code to proceed the searchbard searches */
   document
     .querySelector(".search-bar")
     .addEventListener("keyup", function (event) {
@@ -49,5 +59,6 @@ let weather = {
         weather.search();
       }
     });
+    
   
   weather.fetchWeather("Denver");
